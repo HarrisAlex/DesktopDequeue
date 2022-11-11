c14e8d0d3a4a71375ae8a98d0ddc6c8206594cac
HarrisAlex <alexharris7639@gmail.com>
2022-11-11 15:59:25 -0500

    initial

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+var lan_ip = null;
+
+//Get local IP
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4;
+
+        if (net.family === familyV4Value && !net.internal) {
+            if (net.address.toString().includes('192')) {
+                lan_ip = net.address;
+            }
+        }
+    }
+}
+
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+      width: 800,
+      height: 800,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+
+    window.loadFile('index.html');
+}
+
+
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    });
+
+    ipcMain.on("get-ip", (event, value) => {
+        event.returnValue = lan_ip;
+    });
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
